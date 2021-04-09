af9e1eff0a1cc2ba06666c82c6f8858f8a71679f
Akshitaravi <74477746+Akshitaravi@users.noreply.github.com>
2021-04-09 16:54:43 +0800

    Add files via upload

---
+++ Assets/Script/TeacherMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeacherMenu : MonoBehaviour
+{
+    public void SummaryReport()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreateAssignment()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+
+}
